fc6a20b386f92fa5811da9bd35f018d8118d3a37
parniame <pzedbaz@gmail.com>
2024-07-01 01:17:32 +0330

    Add Alert version

--- js/javascript.js
+++ js/javascript.js
@@ -1,50 +1,45 @@
+var error_ = false;
 document.addEventListener("submit", function (e) {
-  const message = document.getElementById("message");
   const birthDateString = document.getElementById("birth-date").value;
 
-  age = CheckAge(birthDateString, message);
+  age = CheckAge(birthDateString);
 
   const bootcampCode = document.getElementById("bootcamp-code").value;
-  CheckBootCampCode(bootcampCode, message);
-  if (!("alert-danger" in message.classList)) {
+  CheckBootCampCode(bootcampCode);
+  if (!error_) {
     const firstName = document.getElementById("first-name").value;
     const lastName = document.getElementById("last-name").value;
 
-    message.classList.add("alert-success");
-    message.textContent =
+    alert(
       "Your name is " +
-      firstName +
-      " " +
-      lastName +
-      " And You are " +
-      age +
-      "years old!";
+        firstName +
+        " " +
+        lastName +
+        " And You are " +
+        age +
+        "years old!"
+    );
   }
   e.preventDefault();
 });
-function CheckAge(birthDateString, message) {
+function CheckAge(birthDateString) {
   if (birthDateString) {
     const birthDate = new Date(birthDateString);
     const today = new Date();
     const age = today.getFullYear() - birthDate.getFullYear();
 
     if (age < 18) {
-      message.textContent = " You must be atleast 18!\n";
-      CheckClass(message, "alert-danger");
+      alert("You must be atleast 18!");
+      error_ = true;
     }
     return age;
   }
 }
-function CheckBootCampCode(bootcampCode, message) {
+function CheckBootCampCode(bootcampCode) {
   if (bootcampCode) {
     if (bootcampCode != 114) {
-      CheckClass(message, "alert-danger");
-      message.textContent += "Bootcamp Code is not acceptable\n";
+      alert("Bootcamp Code is not acceptable");
+      error_ = true;
     }
-  }
-}
-function CheckClass(obj, class_) {
-  if (!(class_ in obj.classList)) {
-    obj.classList.add("alert-danger");
   }
 }
